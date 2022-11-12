f8173cb403de3375e79f410e92be69362ca5b583
Rainer Piil <piilrainer@gmail.com>
2022-11-12 09:39:15 +0200

    update env

--- pages/dashboard/add.jsx
+++ pages/dashboard/add.jsx
@@ -9,14 +9,17 @@ import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
 import Link from 'next/link'
 import { useState } from "react"
+import Box  from "@mui/material/Box"
 import AddNewEntryBox from "../../src/Components/AddNewEntryBox"
 
 
 function Add() {
-
   return (
     <Container>
-      <h2>Kommentaaride lisamine</h2>      
+      <h2> <Box sx={{ display: "flex", }}>
+        <h2>Kommentaaride lisamine</h2>
+        <Button variant="outlined" href='/dashboard' style={{ height: "20%", marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}>Dashboard</Button>
+      </Box></h2>
       <div>
         <AddNewEntryBox />
       </div>
--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from "@mui/material/Container"
 import CommentsListItem from '../../src/Components/CommentsListBox'
 import AddIcon from '@mui/icons-material/Add';
@@ -7,23 +7,63 @@ import { Box } from '@mui/material';
 import Tabs from '@mui/material/Tabs';
 import { ResetTvRounded } from '@mui/icons-material';
 import prisma from '../../src/db/prisma';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import Tab from '@mui/material/Tab';
+import TabPanel from '@mui/lab/TabPanel';
+import {getSession } from "next-auth/react"
 
-export const getServerSideProps = async ({ req }) => {
-  const entrys = await prisma.entrys.findMany();
-  console.log(entrys.length);
-  return { props: { entrys } }
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  if (session) {
+    const entrys = await prisma.entrys.findMany();
+    return {
+      props: { session,entrys}
+    }
+  }
+  else {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    }
+  }
 }
+
 
 function Dashboard(props) {
   const { entrys } = props;
+  const [value, setValue] = useState("1")
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
   return (
     <div>
       <Container>
         <div>
           <h2>Dashboard</h2>
-          <Button variant="contained" startIcon={<AddIcon />}>Lisa uus</Button>
+          <Button variant="contained" href='/dashboard/add' startIcon={<AddIcon />}>Lisa uus</Button>
           <small style={{ marginLeft: "auto", display: "block" }}>Sissekandeid kokku: {entrys.length}</small>
         </div>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Juhendaja kommentaarid" value="1" />
+              <Tab label="Üksikud sissekanded" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            {entrys.length != 0 ? entrys.map(() => {
+              return (
+                <CommentsListItem key={entrys.lenght} />
+              )
+            }) : "Sissekandeid ei leitud"}
+          </TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+        </TabContext>
         <Box
           sx={{
             display: 'flex',
@@ -36,11 +76,7 @@ function Dashboard(props) {
             borderRadius: 1,
           }}
         >
-          {entrys.length !=0 ? entrys.map(() => {
-            return (
-              <CommentsListItem key={entrys.lenght} />
-            )
-          }) : "Sissekandeid ei leitud"}
+
         </Box>
       </Container>
     </div>
