dc0e4cfca8b894b8659f89476b32269c7e21292c
Rainer Piil <piilrainer@gmail.com>
2022-11-13 22:45:02 +0200

    favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,11 +3,15 @@ import { SnackbarProvider } from 'notistack';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { SessionProvider } from "next-auth/react"
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps:{ session, ...pageProps },}) {
+function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
 
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+      </Head>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <SnackbarProvider>
           <Component {...pageProps} />
